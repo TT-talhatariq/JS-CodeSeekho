7a3be63ceec4fac5b52c740b27162eaec4426110
Talha Tariq <talhacoder01@gmail.com>
2023-09-16 15:35:00 +0500

    Done

---
+++ Intermediate/script.js
@@ -0,0 +1,92 @@
+"use strict";
+
+let name = "Talha";
+let age = 21;
+let role = "SE";
+
+// // Structure
+// for (let start = 1; start <= 10; start = start + 1) {
+//   console.log("Start: ", start, " Name: ", name);
+// }
+
+// Numbers
+// for (let start = 0; start <= 20; start = start + 1) {
+//   console.log(start);
+// }
+
+// console.log("After Loop");
+
+// Print a Table
+/*
+    2 * 1 = 2
+    2 * 2 = 4
+    2 * 3 = 8
+*/
+
+// console.log(`2 * ${1} = ${2 * 1}`);
+
+// console.log(`2 * ${2} = ${2 * 2}`);
+
+// console.log(`2 * ${3} = ${2 * 3}`);
+
+// for (let start = 1; start <= 10; start = start + 1) {
+//   console.log(`10 * ${start} = ${10 * start}`);
+// }
+
+// Fibonacci Sequence
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ......
+
+// let firstNumber = 0;
+// let secondNumber = 1;
+// for (let start = 0; start < 20; start++) {
+//   let result = firstNumber + secondNumber;
+
+//   firstNumber = secondNumber;
+//   secondNumber = result;
+
+//   console.log(result);
+// }
+
+for (let outer = 1; outer <= 5; outer++) {
+  for (let i = 0; i < 5; i++) {
+    console.log(name + " ", i);
+  }
+}
+
+let age2 = 22;
+console.log(age);
+
+let uni = "PUCIT";
+uni = "Naseeha";
+
+console.log(uni);
+
+if (age < 18) {
+  console.log("Not Eligible");
+} else {
+  console.log("Eligible");
+}
+
+for (let i = 10; i > -2; i--) {
+  console.log(20 / i);
+  // i = 0
+  // 20 / 0
+}
+
+/*
+
+Syntax: Voilation of Rules
+Logical: Mistake in Code
+Runtime: Unexpected input while executing code.
+
+*/
+
+
+
+// Task 1
+...
+
+// Task 2
+...
+
+// Task
